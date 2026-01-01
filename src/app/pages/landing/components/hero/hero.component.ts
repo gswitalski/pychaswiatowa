83fbee4df5636eeb722277cc24b2515a1a3d7f4d
Grzegorz Świtalski <gswitalski@gmail.com>
2026-01-01 22:59:05 +0100

    feat: integrate NgOptimizedImage for improved image handling in hero component; update image attributes and styles for better responsiveness

--- src/app/pages/landing/components/hero/hero.component.ts
+++ src/app/pages/landing/components/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'pych-hero',
     standalone: true,
-    imports: [RouterLink, MatButtonModule],
+    imports: [RouterLink, MatButtonModule, NgOptimizedImage],
     templateUrl: './hero.component.html',
     styleUrl: './hero.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
